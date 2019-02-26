6eac1393cf7d783dd70511cb717d32a221e77c04
McNultyPT <mcnultypt@gmail.com>
2019-02-26 11:00:34 -0800

    demo complete

--- roles/roles-router.js
+++ roles/roles-router.js
@@ -1,28 +1,109 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const knex = require("knex");
 
-router.get('/', (req, res) => {
+const knexConfig = {
+  client: "sqlite3",
+  useNullAsDefault: true,
+  connection: {
+    filename: "./data/rolex.db3"
+  }
+};
+const db = knex(knexConfig);
+
+router.get("/", (req, res) => {
   // get the roles from the database
-  res.send('Write code to retrieve all roles');
+  db("roles")
+    .then(roles => {
+      res.status(200).json(roles);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // retrieve a role by id
-  res.send('Write code to retrieve a role by id');
+  db("roles")
+    .where({ id: req.params.id })
+    .first()
+    .then(role => {
+      res.status(200).json(role);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
-router.post('/', (req, res) => {
+// router.get('/:id', async (req, res) => {
+//   const id = req.params.id
+//   try {
+//     const role = await db('roles')
+//     .where({id})
+//     return res.status(200).json(role)
+//   } catch(error) {
+//     console.log(error)
+//     res.status(500).json(error)
+//   }
+// });
+
+router.post("/", (req, res) => {
   // add a role to the database
-  res.send('Write code to add a role');
+  db("roles")
+    .insert(req.body)
+    .then(ids => {
+      const [id] = ids;
+
+      db("roles")
+        .where({ id })
+        .first()
+        .then(role => {
+          res.status(200).json(role);
+        });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update roles
-  res.send('Write code to modify a role');
+  db("roles")
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(count => {
+      if (count > 0) {
+        db("roles")
+          .where({ id: req.params.id })
+          .first()
+          .then(role => {
+            res.status(200).json(role);
+          });
+      } else {
+        res.status(404).json({ message: "Role not found." });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // remove roles (inactivate the role)
-  res.send('Write code to remove a role');
+  const id = req.params.id;
+
+  db("roles")
+    .where({ id })
+    .del()
+    .then(count => {
+      if (count > 0) {
+        res.status(204).json({ message: "Role has been deleted." });
+      } else {
+        res.status(404).json({ message: "Role not found." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
